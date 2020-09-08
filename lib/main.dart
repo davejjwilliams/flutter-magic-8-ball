604c8ea407c4d0254c64d88308bf7b5f49c6677a
Dave Williams <davejjwilliams@gmail.com>
2020-09-08 12:30:38 +0200

    App Upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: BallPage(),
       ),
     );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Ask Me Anything',
+        ),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int answer = 1;
+
+  void randomiseAnswer() {
+    setState(() {
+      answer = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$answer.png'),
+        onPressed: () {
+          randomiseAnswer();
+        },
+      ),
+    );
+  }
+}
